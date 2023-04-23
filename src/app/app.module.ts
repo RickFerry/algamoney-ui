1416c60dda036ed03b96b1dcfe3d5e3d2b50fa92
RickFerry <rick_ferry1@hotmail.com>
2023-04-22 23:39:39 -0300

    Fix imports and libraries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
+import { TooltipModule } from 'primeng/tooltip';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { TableModule } from 'primeng/table';
 import { TabViewModule } from 'primeng/tabview';
-import { TooltipModule } from 'primeng/tooltip';
 import { AppComponent } from './app.component';
 
 import { CalendarModule } from 'primeng/calendar';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { SelectButtonModule } from 'primeng/selectbutton';
 import { AppRoutingModule } from './app-routing.module';
 import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.component';
 import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { DropdownModule } from 'primeng/dropdown';
-import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
   declarations: [
@@ -23,20 +26,22 @@ import { InputNumberModule } from 'primeng/inputnumber';
     LancamentosPesquisaComponent,
     NavbarComponent,
     PessoasPesquisaComponent,
-    LancamentoCadastroComponent
-  ],
+    LancamentoCadastroComponent,
+    PessoaCadastroComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ButtonModule,
     InputTextModule,
-    TableModule,
     TabViewModule,
-    TooltipModule,
     CalendarModule,
     SelectButtonModule,
     DropdownModule,
-    InputNumberModule
+    InputNumberModule,
+    TableModule,
+    InputMaskModule,
+    TooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
