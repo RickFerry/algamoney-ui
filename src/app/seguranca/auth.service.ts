0d9c723b908b344f2ac0a3b8c9c0749bd0364c0f
RickFerry <rick_ferry1@hotmail.com>
2023-04-25 16:45:23 -0300

    Carregar jwtPayload funcionando

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -3,34 +3,41 @@ import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   loginUrl = 'https://finance-api.up.railway.app/login';
-  jwtPayload: any
+  jwtPayload: any;
 
-  constructor(
-    private http: HttpClient,
-    private jwt: JwtHelperService
-    ) { }
+  constructor(private http: HttpClient, private jwt: JwtHelperService) {
+    this.carregarToken();
+  }
 
   async login(usuario: string, senha: string): Promise<any> {
-    const headers = new HttpHeaders({'Content-Type':'application/json'})
-    const body = {"email":usuario, "senha":senha}
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { email: usuario, senha: senha };
     try {
-      return this.http.post(this.loginUrl, body, { headers })
-      .subscribe((resp: any) => {
-        console.log(resp['token'])
-        this.armazenaToken(resp['token'])
-        console.log(this.jwtPayload)
-      })
+      return this.http
+        .post(this.loginUrl, body, { headers })
+        .subscribe((resp: any) => {
+          console.log(resp['token']);
+          this.armazenaToken(resp['token']);
+          console.log(this.jwtPayload);
+        });
     } catch (response) {
       console.log(response);
     }
   }
 
-  private armazenaToken(token: string){
-    this.jwtPayload = this.jwt.decodeToken(token)
+  private armazenaToken(token: string) {
+    this.jwtPayload = this.jwt.decodeToken(token);
+    localStorage.setItem('token', token);
+  }
+
+  private carregarToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.armazenaToken(token);
+    }
   }
 }
